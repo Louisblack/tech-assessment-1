3361fac738873c747ecfa6d5b3e3fd72e35126cf
Louis Houghton <louis@louishoughton.com>
2018-07-16 08:42:08 +0100

    Add repository so we can fake it in tests

---
+++ src/main/java/com/louishoughton/modulr/account/AccountRepository.java
@@ -0,0 +1,11 @@
+package com.louishoughton.modulr.account;
+
+public interface AccountRepository {
+
+    /**
+     * Retrieves the requested account from the repository
+     * @param accountNumber The unique account number for the requested account
+     * @return The Account with the unique account number provided
+     */
+    Account get(long accountNumber);
+}
